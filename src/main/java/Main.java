98c7ee6a862c1d894698e86776748db0654ef748
Oleg Rachitsky <Oleg.Rachitsky1@ibm.com>
2021-05-06 17:54:21 +0300

    Use version 5.8.2

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public @interface A {
+	}
+
+	public void main(@A String qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq,
+			@A String qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq,
+			@A String qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq) {
+		//
+	}
+}
